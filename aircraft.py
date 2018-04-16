e21d5fd37770572a81efa4ef43c886a3131a2a49
Nick <nick.delgado@gmail.com>
2018-04-16 13:43:03 -0400

    Improved aircraft model

--- aircraft.py
+++ aircraft.py
@@ -3,35 +3,44 @@
 class Aircraft():
     def __init__(self, tailnumber):
         self.reg = tailnumber
-        self._name = ''
+        self._name = None
+        self._cruising_speed = None
+        self._cruising_altitude = None
+        self._weight = None
 
     @property
     def name(self):
         return self._name
 
     @name.setter
-    def name(self, value = ''):
+    def name(self, value = None):
         #check if it is the correct length
         self._name = value
 
-    def setProperty(self, which, value):
-        #Right now all these functions are being called
-        switcher = {
-                'name'  : self.__setNameProperty(value),
-                'cruising-altitude' : self.__setCruisingAltitudeProperty(value)
-                }
-        return switcher.get(which, 'error')
+    @property
+    def cruising_speed(self):
+        return self._cruising_speed
 
-    def __setNameProperty(self, name):
-        self.name = name
-        print(self.name)
-        return True
+    @cruising_speed.setter
+    def cruising_speed(self, value = None):
+        #check if it is the correct length
+        self._cruising_speed = value
 
-    def __setWeightProperty(self, name):
-        return False
 
-    def __setCruisingSpeedProperty(self, name):
-        return False
+    @property
+    def cruising_altitude(self):
+        return self._cruising_altitude
 
-    def __setCruisingAltitudeProperty(self, name):
-        return False
+    @cruising_altitude.setter
+    def cruising_altitude(self, value = None):
+        #check if it is the correct length
+        self._cruising_altitude = value
+
+    @property
+    def weight(self):
+        return self._weight
+
+    @weight.setter
+    def weight(self, value = None):
+        #check if it is the correct length
+        self._weight = value
