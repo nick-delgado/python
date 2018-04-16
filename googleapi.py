3ee84f7e64e8fd6f62118ee77a7ca0e10d81f753
Nick <nick.delgado@gmail.com>
2018-04-16 13:57:18 -0400

    Presented more questions with google api in comments

--- googleapi.py
+++ googleapi.py
@@ -1,12 +1,23 @@
 import googlemaps
 from datetime import datetime
 
+#API-KEY=  AIzaSyDV93OxljDchBbiBHjfjfbhpMMIyn3aYs8
+
 gmaps = googlemaps.Client(key='AIzaSyDV93OxljDchBbiBHjfjfbhpMMIyn3aYs8')
 
 reverse_geocode_result = gmaps.reverse_geocode((35.10873, -86.11313))
+#what is returned? 
+#what can be parsed? 
+#is it always consistent? 
+#does it change based on location? 
+# in higher concentration areas of POI is there a filtering system to decrease results or to filter results to what we need?
+
 geocode_result = gmaps.geocode('37363')
+#can i return an geolocation from this?
+#then use that geopoint to find airports within that radius?
+#what about very  large zip code areas? 
+#what about doing a functions that if no top 10 airports are found then increase the mileage radius search?
 print(reverse_geocode_result)
 print(geocode_result)
 
-#API-KEY=  AIzaSyDV93OxljDchBbiBHjfjfbhpMMIyn3aYs8
 
