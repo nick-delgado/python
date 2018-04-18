23b91beeb0a269a5d732192bce577d743b245546
Nick <nick.delgado@gmail.com>
2018-04-18 16:26:51 -0400

    Added updater and weather

---
+++ updater.py
@@ -0,0 +1,4 @@
+# create this microservice that will update static, inmovable objects but that have
+#properties that get updated, such as weather (wind, temperature)...
+#keep historical data? is it needed? no..
+#update every 5-15min? 
---
+++ weather.py
@@ -0,0 +1,6 @@
+from noaa_sdk import noaa
+
+#definte the weather as a 'static' object...store it in database and update to database
+def weather():
+    def __init__(self):
+        
