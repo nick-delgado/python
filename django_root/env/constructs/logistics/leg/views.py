f76342506103be861fb3d54550c029b1161f6f17
Nick Delgado <nick@Nicks-MBP.hsd1.tn.comcast.net>
2018-04-23 22:28:51 -0400

    Add another aircraft sample

--- django_root/env/constructs/logistics/leg/views.py
+++ django_root/env/constructs/logistics/leg/views.py
@@ -5,7 +5,8 @@ import requests
 # Create your views here.
 def home(request):
     data = ''
-    for i in range(0,1):
+    for i in range(0,9):
         response = requests.get('http://127.0.0.1:8000/aircrafts/')
+        data = data + str(response.json())
 
     return HttpResponse("Hello, world" + str(data))
