fed0710ab6394e31f6f971054fe8944b23ba0680
Nick <nick.delgado@gmail.com>
2018-04-25 19:09:59 -0400

    Add airport profile

--- django_root/env/elements/element/aircraft_profile/models.py
+++ django_root/env/elements/element/aircraft_profile/models.py
@@ -3,3 +3,4 @@ from django.db import models
 # Create your models here.
 class AirportProfile(models.Model):
     name = models.CharField(max_length=40)
+    max_cargo_weight = models.IntegerField(default=0)
--- django_root/env/elements/element/airport/models.py
+++ django_root/env/elements/element/airport/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Airport(models.Model):
+    code = models.CharField(max_length=5)
+    name = models.CharField(max_length=50)
