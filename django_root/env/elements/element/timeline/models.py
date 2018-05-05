7f17f578dbfa9f7104ae1d18660658f29380dad1
Nick <nick.delgado@gmail.com>
2018-05-05 11:07:01 -0400

    Created timeline app that will be integrated into the time stream and added class models

---
+++ django_root/env/elements/element/timeline/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+class timeline(models):
+    pass
+
+class year(models):
+    #has events check
+    pass
+
+class month(object):
+    pass
+
+class week(object):
+    pass
+
+class day(object):
+    pass
+
+class hour(object):
+    pass
+
+class minute(object):
+    pass
+
+class event(object):
+    #event can be associated to any of the time elements above(?)
+    pass
+class quarter(object):
+    pass
+
+class semester(object):
+    pass
+
